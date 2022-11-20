70ed524a12b3544a735a054a5b1d064b3fbc6a60
Roman <86300574+simbullar@users.noreply.github.com>
2022-11-20 18:21:02 +0200

    Add files via upload

--- bounce.py
+++ bounce.py
@@ -3,8 +3,9 @@ import random
 import time
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]
@@ -13,6 +14,14 @@ class Ball:
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -20,8 +29,10 @@ class Ball:
         if pos[1] <= 0:
             self.y = 1
         if pos[3] >= self.canvas_height:
-            self.y = -1
-        if pos[1] <= 0:
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
+            self.y = -3
+        if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
@@ -33,9 +44,22 @@ class Paddle:
         self.canvas.move(self.id, 200, 300)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
 
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def turn_left(self, evt):
+        self.x = -2
+
+    def turn_right(self, evt):
+        self.x = 2
 
 tk = Tk()
 tk.title("Game")
@@ -46,13 +70,13 @@ canvas.pack()
 tk.update()
 
 
-
-ball = Ball(canvas, 'red')
 paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
 
 while 1:
-    ball.draw()
-    paddle.draw()
+    if ball.hit_bottom == False:
+        ball.draw()
+        paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
